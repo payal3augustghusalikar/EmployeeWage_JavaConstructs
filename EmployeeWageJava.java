14f5abc5f8b307904a1b015f277c4f9404d44efe
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-10-24 23:40:04 +0530

    Added : employee attendence

--- EmployeeWageJava.java
+++ EmployeeWageJava.java
@@ -1,5 +1,11 @@
 public class EmployeeWageJava {
 	public static void main(String[] args) {
-			System.out.println("Welcome to employee wage computation program by java");
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
+
