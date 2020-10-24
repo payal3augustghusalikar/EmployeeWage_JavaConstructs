d3f95929f3343c3f01d3f6782a38d4e9b51e8842
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-10-24 23:55:31 +0530

    Added : part time hour

--- EmployeeWageJava.java
+++ EmployeeWageJava.java
@@ -2,6 +2,7 @@ public class EmployeeWageJava {
 	public static void main(String[] args) {
 		// Constants
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		//variables
 		int empHrs = 0;
@@ -10,6 +11,8 @@ public class EmployeeWageJava {
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
+		else if (empCheck == IS_PART_TIME)
+         empHrs = 4;
 		else
 			empHrs = 0;
 		empWage = empHrs * EMP_RATE_PER_HOUR;
